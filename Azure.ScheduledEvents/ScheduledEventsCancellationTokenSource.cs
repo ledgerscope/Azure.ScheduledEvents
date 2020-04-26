7b656d9ace7e02d68e13567a034f25fc36008a50
Matthew Steeples <matthew@ledgerscope.com>
2020-04-26 17:05:31 +0100

    Initial project commit

---
+++ Azure.ScheduledEvents/ScheduledEventsCancellationTokenSource.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Timer = System.Timers.Timer;
+using ElapsedEventArgs = System.Timers.ElapsedEventArgs;
+using System.Diagnostics;
+
+namespace Azure.ScheduledEvents
+{
+    public class ScheduledEventsCancellationTokenSource// : IDisposable
+    {
+        private readonly Timer _t = new Timer(TimeSpan.FromSeconds(2).TotalMilliseconds);
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly ScheduledEventsClient _client;
+        private readonly TimeSpan _noticePeriod = TimeSpan.FromSeconds(30);
+
+        public ScheduledEventsCancellationTokenSource()
+        {
+            _t.Elapsed += t_Elapsed;
+
+            _client = new ScheduledEventsClient();
+        }
+
+        public CancellationToken GetCancellationToken()
+        {
+            return _cancellationTokenSource.Token;
+        }
+
+        private void t_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var document = _client.GetScheduledEvents();
+
+                foreach (var item in document.Events)
+                {
+                    if (item.NotBefore.HasValue)
+                    {
+                        if (item.NotBefore.Value.Add(_noticePeriod) < DateTime.UtcNow)
+                        {
+                            _cancellationTokenSource.Cancel();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Azure.ScheduledEvents/ScheduledEventsClient.cs
@@ -0,0 +1,73 @@
+﻿/* Copyright 2014 Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
+
+using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace Azure.ScheduledEvents
+{
+    public class ScheduledEventsClient
+    {
+        private readonly Uri scheduledEventsEndpoint;
+
+        public ScheduledEventsClient(Uri scheduledEventsEndpoint)
+        {
+            this.scheduledEventsEndpoint = scheduledEventsEndpoint;
+        }
+
+        public ScheduledEventsClient() : this(new Uri("http://169.254.169.254/metadata/scheduledevents?api-version=2019-01-01"))
+        {
+
+        }
+
+        /// <summary>
+        /// Issues a get request to the scheduled events endpoint.
+        ///
+        /// For additional information on possible return status codes and headers,
+        /// Please see here: "https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-scheduled-events"
+        ///
+        /// </summary>
+        /// <returns>The Scheduled Events document</returns>
+        public ScheduledEventsDocument GetScheduledEvents()
+        {
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("Metadata", "true");
+                var json = webClient.DownloadString(scheduledEventsEndpoint);
+
+                var scheduledEventsDocument = JsonConvert.DeserializeObject<ScheduledEventsDocument>(json);
+
+                return scheduledEventsDocument;
+            }
+        }
+
+        /// <summary>
+        /// Issues a post request to the scheduled events endpoint with the given json string
+        ///
+        /// For additional information on possible return status codes and headers,
+        /// Please see here: "https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-scheduled-events"
+        /// </summary>
+        /// <param name="jsonPost">Json string with events to be approved</param>
+        public void ExpediteScheduledEvents(string jsonPost)
+        {
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("Content-Type", "application/json");
+                webClient.UploadString(scheduledEventsEndpoint, jsonPost);
+            }
+        }
+    }
+}
